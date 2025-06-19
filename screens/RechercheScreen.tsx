d8b6f84a2bdad0625e103c0d5cda7f806f6aed37
Lussiez Julien <70692644+JLussiez@users.noreply.github.com>
2025-06-19 14:42:03 +0200

    récupération des données de l'api

--- screens/RechercheScreen.tsx
+++ screens/RechercheScreen.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { getOperateurs } from '../services/bioOperateursApi';
 
 const RechercheScreen = () => {
   const [totalOperateurs, setTotalOperateurs] = useState<string | null>(null);
@@ -8,7 +9,6 @@ const RechercheScreen = () => {
     const fetchData = async () => {
       try {
         const response = await getOperateurs();
-        console.log(response);
         setTotalOperateurs(response.nbTotal);
       } catch (error) {
         console.error('Erreur:', error);
@@ -21,6 +21,11 @@ const RechercheScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Recherche</Text>
+      {totalOperateurs !== null && (
+        <Text style={styles.subtitle}>
+          Nombre total d'opérateurs bio : {totalOperateurs}
+        </Text>
+      )}
     </View>
   );
 };
@@ -36,6 +41,11 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     color: '#333',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#666',
   },
 });
 
