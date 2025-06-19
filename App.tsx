b58ff4af448d64a3b963866340af15bbf37b7898
Lussiez Julien <70692644+JLussiez@users.noreply.github.com>
2025-06-19 09:37:42 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -5,8 +5,7 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -14,7 +13,7 @@ function App() {
   return (
     <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <Text style={styles.text}>Hello World</Text>
     </View>
   );
 }
@@ -22,6 +21,14 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#000',
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 50,
+    color: 'white',
+    marginBottom: 50,
   },
 });
 
