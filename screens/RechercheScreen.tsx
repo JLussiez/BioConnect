1ae656853830285775605c4e064eadc209018b43
Lussiez Julien <70692644+JLussiez@users.noreply.github.com>
2025-06-19 14:09:13 +0200

    Revert "Dev"

--- screens/RechercheScreen.tsx
+++ screens/RechercheScreen.tsx
@@ -1,31 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { getOperateurs } from '../services/bioOperateursApi';
 
 const RechercheScreen = () => {
-  const [totalOperateurs, setTotalOperateurs] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await getOperateurs();
-        setTotalOperateurs(response.nbTotal);
-      } catch (error) {
-        console.error('Erreur:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Recherche</Text>
-      {totalOperateurs !== null && (
-        <Text style={styles.subtitle}>
-          Nombre total d'opérateurs bio : {totalOperateurs}
-        </Text>
-      )}
     </View>
   );
 };
@@ -41,11 +20,6 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     color: '#333',
-    marginBottom: 10,
-  },
-  subtitle: {
-    fontSize: 16,
-    color: '#666',
   },
 });
 
